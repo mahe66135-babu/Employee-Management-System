dfaa02132ca9b0df1eeff2520d18a18479107ed9
Surya <196r1a0521@gmail.com>
2026-08-07 12:32:27 +0530

    add Register debug

--- ems-frontend/src/pages/auth/Register.jsx
+++ ems-frontend/src/pages/auth/Register.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
-import API from "../api/axiosConfig";
+import API from "../../api/axiosConfig";
 
 function Register() {
     const navigate = useNavigate();
